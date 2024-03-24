69d7c9c075a8be6e9617f88b98252527d900512a
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-24 21:02:17 +0100

    Update web.php
    We maken een get route waar we de id ophalen en gebaseerd daarop de edit_post functie kunnen gebruiken om dan natuurlijk onze edit pagina te tonen, hetzelfde voor de update functie. Met de nodige naam verwijzingen dat we eerder in de HTML gebruikt hebben om naar toe te routen. Allerlaatst een delete route met dezelfde logica, ik heb hier gebruik gemaakt van delete inplaats van get omdat ik anders een /delete route zou moeten gebruiken en dacht niet dat dat nodig was.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,16 @@ Route::get('/home', function (){
     return redirect('/');
 });
 
+Route::get('/status/{id}', [PostController::class, 'status'])->name('status');
 
 Route::middleware('auth')->group(function () {
     Route::get('/create-post', [PostController::class, 'create_post'])->name('create-post');
     Route::post('/create-post', [PostController::class, 'store'])->name('create-post.store');
+
+    Route::get('/edit-post/{id}', [PostController::class, 'edit_post'])->name('edit-post');
+    Route::post('/edit-post/{id}', [PostController::class, 'update'])->name('edit-post.update');
+    Route::delete('/edit-post/{id}', [PostController::class, 'delete'])->name('edit-post.delete');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
