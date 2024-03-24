ab9443a2130fcbda44846d420cc1160419fde843
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-24 02:14:49 +0100

    Update model Post
    Als we aan fillables willen doen kunnen we file_path er ook bij zetten.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
     use HasFactory;
     use SoftDeletes;
     protected $fillable = [
-        'title', 'content', 'user_id'
+        'title', 'content', 'user_id', 'file_path'
     ];
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
