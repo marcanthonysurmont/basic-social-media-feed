47887bece800b6d5fd47a48bb8144e6c01b47f73
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-24 20:44:35 +0100

    Post-preview add edit-logo
    Als je ingelogd bent en je ziet jou post zal je een edit logotje zien dat je zal sturen naar de post edit scherm en de ID van die post zal meegeven.

--- resources/views/components/post-preview.blade.php
+++ resources/views/components/post-preview.blade.php
@@ -1,23 +1,30 @@
 @props(['post'])
 
 <br>
-<div class="block max-w-[877px] mx-auto bg-gray-800 text-white rounded-lg overflow-hidden shadow-lg">
-    <a href="#">
-        <div class="p-6">
+<div class="block max-w-[877px] mx-auto bg-white text-gray-800 rounded-lg overflow-hidden shadow-lg relative"> <!-- Added relative positioning -->
+    <a href="#" class="block relative">
+        <div class="p-6 relative"> <!-- Added relative positioning -->
             <p class="text-sm font-semibold text-gray-400 mb-2">{{$post->user->name}}</p>
             <h2 class="text-xl font-semibold mb-2">{{$post->title}}</h2>
+            @if(Auth::id() === $post->user_id)
+                <div class="absolute top-0 right-0 transform p-4"> <!-- Positioned edit logo -->
+                    <a href="{{ route('edit-post', ['id' => $post->id]) }}"><x-edit-logo></x-edit-logo></a>
+                </div>
+            @endif
         </div>
         @if(!empty($post->file_path))
             <img src="{{asset('storage/' . $post->file_path)}}" alt="{{$post->title}}" class="w-full h-auto mb-6">
         @endif
     </a>
     <div class="p-6 lg:pt-0 flex">
-        <a href="" class="inline-block"><x-unfilled-heart></x-unfilled-heart></a>
+        <a href="#" class="inline-block"><x-unfilled-heart></x-unfilled-heart></a>
         <span class="text-gray-400 ml-3">0 likes</span>
 
-        <a href="" class="inline-block"><x-comment-logo></x-comment-logo></a>
+        <a href="#" class="inline-block"><x-comment-logo></x-comment-logo></a>
         <span class="text-gray-400 ml-3">0 comments</span>
     </div>
 </div>
+
+
 
 
