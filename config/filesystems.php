9b378dc78092d0810526881b900addb00fd96768
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-24 02:10:26 +0100

    Update filesystems.php for file storage
    Dit heb ik online gevonden maar dit zorgt ervoor dat we uploads op een een bepaalde plek gaan opslaan en als we die uploads willen gaan vinden hebben we de URL daarvoor ook.

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,12 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'uploads' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/uploads'),
+            'url' => env('APP_URL').'/storage/uploads', // Define the URL for accessing uploaded files
+            'visibility' => 'public',
+        ],
 
     ],
 
