900744a69e591322a556078304001f9046ed9b0a
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-26 00:16:44 +0100

    Added addComment PostController
    Een functie addComment simpel gewoon om comments toe te voegen aan de database doormiddel van een post_id en de de ingebouwde user_id auth.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use App\Models\Post;
@@ -124,17 +125,39 @@ class PostController extends Controller
 
             if(!$post->hasLiked($user))
             {
-                $post->likedByUsers()->attach(Auth::id());
+                $post->likedByUsers()->attach($user->id);
             }else
             {
-                $post->likedByUsers()->detach(Auth::id());
+                $post->likedByUsers()->detach($user->id);
             }
 
             return redirect()->back();
 
         }catch(ModelNotFoundException)
         {
-            redirect('/login')->with('error', 'You need to be logged in for that');
+            return redirect('/login')->with('error', 'You need to be logged in for that');
         }
+    }
+
+    function addComment(int $post_id, Request $request)
+    {
+        $request->validate([
+            'content' => 'required'
+        ]);
+
+        try
+        {
+            $comment = new Comment();
+            $comment->post_id = $post_id;
+            $comment->user_id = Auth::id();
+            $comment->comment = $request->input('content');
+            $comment->save();
+
+        }catch(ModelNotFoundException)
+        {
+            return redirect('/login')->with('error', 'You need to be logged in for that');
+        }
+
+        return redirect()->back();
     }
 }
