4c1061678930d972ea1377328f90ae55d83c6a58
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-26 00:25:20 +0100

    Update web.php
    We vragen hier de id op van de URL en geven die mee aan onze net gemaakte addComment functie en we geven deze route de naam status.comment zodat onze form weet naar wat het moet verwijzen.

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::middleware('auth')->group(function () {
 
     Route::post('/', [PostController::class, 'like'])->name('home.like');
 
+    Route::post('/status/{id}', [PostController::class, 'addComment'])->name('status.comment');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
