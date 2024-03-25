01946087bf53a0279fdd8a756805db849a5e6b01
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-26 00:14:57 +0100

    Create migration comments
    We maken een tabel comments aan waar we 2 foreign keys zetten omdat we een post_id nodig gaan hebben om te zien op welke post welke user een comment heeft gezet. Ook hebben we timestamps om te zien wanneer een post gemaakt is of geupdate en softdeletes voor als we de comment verwijderen

---
+++ database/migrations/2024_03_25_195955_create_comments.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->references('id')->on('posts')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
+            $table->string('comment');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_has_comments');
+    }
+};
