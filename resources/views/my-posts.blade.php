4a60c6b4c2109ee07fae0e91026ac9ed35d89f34
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-26 19:14:42 +0100

    Deleted edit function my-post

--- resources/views/my-posts.blade.php
+++ resources/views/my-posts.blade.php
@@ -9,11 +9,6 @@
                     <div class="p-4"> <!-- Adjust the height as per your requirement -->
                         <p class="text-sm font-semibold text-gray-400 mb-1" style="word-break: break-word">{{ $post->user->name }}</p>
                         <h2 class="text-lg font-semibold mb-2" style="word-break: break-word">{{ Str::substr($post->title, 0, 22)}}@if(strlen($post->title) > 22)...@endif</h2>
-                        @if(Auth::id() === $post->user_id)
-                            <div class="absolute top-0 right-0 transform p-2">
-                                <a href="{{ route('edit-post', ['id' => $post->id]) }}"><x-edit-logo></x-edit-logo></a>
-                            </div>
-                        @endif
                     </div>
                 </a>
             </div>
