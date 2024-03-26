3b94a4e8cbb15f77f3a624b06a86eba69a24fe94
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-26 19:12:56 +0100

    Added flashes with design
    Als er in de sessie error of success bericht is zal dit juist getoont worden bovenaan de pagina

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,41 @@
 
             <!-- Page Content -->
             <main>
+                @if(session('error'))
+                    <div class="flex justify-center">
+                        <div class="max-w-md w-full mt-8"> <!-- Adjusted margin -->
+                            <div id="alert" class="bg-danger-100 border border-danger-200 text-danger-700 px-4 py-3 rounded relative"> <!-- Adjusted padding -->
+                                <span class="block sm:inline">{{ session('error') }}</span>
+                                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                                <button type="button" onclick="document.getElementById('alert').style.display = 'none';">
+                                    <svg class="fill-current h-6 w-6 text-error-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <title>Close</title>
+                                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                                    </svg>
+                                </button>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+                @if(session('success'))
+                    <div class="flex justify-center">
+                        <div class="max-w-md w-full mt-6">
+                            <div id="alert" class="bg-success-100 border border-success-200 text-success-700 px-4 py-3 rounded relative" role="alert">
+                                <span class="block sm:inline">{{ session('success') }}</span>
+                                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                                <button type="button" onclick="document.getElementById('alert').style.display = 'none';">
+                                    <svg class="fill-current h-6 w-6 text-success-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <title>Close</title>
+                                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                                    </svg>
+                                </button>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                @endif
                 {{ $slot }}
                 <br>
             </main>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,17 +14,34 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-21 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div>
+            <a href="/">
+                <x-application-logo class="w-21 h-20 fill-current text-gray-500" />
+            </a>
         </div>
+
+        @if(session('error'))
+            <div class="max-w-sm w-full mt-6">
+                <div id="alert" class="bg-danger-100 border border-danger-200 text-danger-700 px-3 py-2 rounded relative">
+                    <span class="block sm:inline pr-6">{{ session('error') }}</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-3 py-2">
+                        <button type="button" onclick="document.getElementById('alert').style.display = 'none';">
+                            <svg class="fill-current h-5 w-5 text-error-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <title>Close</title>
+                                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                            </svg>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        @endif
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
     </body>
 </html>
