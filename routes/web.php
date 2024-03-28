51b6e6b26d36893cbbe39626e91286b574d0ccf4
Marc Anthony <marc.surmont@student.kdg.be>
2024-03-28 23:58:16 +0100

    Update web.php
    Als je in de URL een waarde gaf dat geen nummer was kwam je op het laravel error scherm terecht waar je allemaal dingen kan zien die niet voor de gebruiker bestemt is dus door dit toe tevoegen en als je dan een letter in de url zet waar ID zou moeten staan krijg je gewoon een 404 pagina

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,19 @@ Route::post('/create-post', [PostController::class, 'store'])->name('create-post
 
 Route::get('/my-posts', [PostController::class, 'getUserPosts'])->name('my-posts');
 Route::get('/my-posts/recently-deleted', [PostController::class, 'recently_deleted'])->name('recently-deleted');
-Route::get('/my-posts/recently-deleted/{id}', [PostController::class, 'restore'])->name('recently-deleted.restore');
+Route::get('/my-posts/recently-deleted/{id}', [PostController::class, 'restore'])->name('recently-deleted.restore')->where('id', '[0-9]+');
 
-Route::post('/edit-post/{id}', [PostController::class, 'update'])->name('edit-post.update');
-Route::delete('/edit-post/{id}', [PostController::class, 'delete'])->name('edit-post.delete');
+Route::post('/edit-post/{id}', [PostController::class, 'update'])->name('edit-post.update')->where('id', '[0-9]+');
+Route::delete('/edit-post/{id}', [PostController::class, 'delete'])->name('edit-post.delete')->where('id', '[0-9]+');
 
 Route::post('/', [PostController::class, 'like'])->name('home.like');
 
-Route::post('/status/{id}', [CommentController::class, 'add'])->name('status.comment');
-Route::delete('/status/{id}', [CommentController::class, 'delete'])->name('status.delete');
+Route::post('/status/{id}', [CommentController::class, 'add'])->name('status.comment')->where('id', '[0-9]+');
+Route::delete('/status/{id}', [CommentController::class, 'delete'])->name('status.delete')->where('id', '[0-9]+');
 
 Route::middleware('auth')->group(function () {
     Route::get('/create-post', [PostController::class, 'create_post'])->name('create-post');
-    Route::get('/edit-post/{id}', [PostController::class, 'edit'])->name('edit-post');
+    Route::get('/edit-post/{id}', [PostController::class, 'edit'])->name('edit-post')->where('id', '[0-9]+');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
